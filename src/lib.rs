9e03b8a9189f75537db58e6bb8ce0af5fef88ae3
lxbc135 <93529090+lxbc135@users.noreply.github.com>
2026-02-28 10:37:43 -0500

    chore: add consumer test config

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,17 @@ use pyo3::prelude::*;
 #[pymodule]
 mod pyo3_test {
     use pyo3::prelude::*;
+    use uuid::Uuid;
 
     /// Formats the sum of two numbers as string.
     #[pyfunction]
     fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
         Ok((a + b).to_string())
+    }
+
+    /// Generates a random UUID v4.
+    #[pyfunction]
+    fn generate_uuid() -> String {
+        Uuid::new_v4().to_string()
     }
 }
